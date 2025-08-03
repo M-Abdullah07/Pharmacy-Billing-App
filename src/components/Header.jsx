3920d8d73bf07f9deb09ce9028b33da564c086d2
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-08-04 02:14:42 +0500

    feat: signout and avatar shown in sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 import React from "react";
+import { Button } from "./ui/button";
+import { SidebarTrigger } from "./ui/sidebar";
 
 export default function Header({ username, onLogout }) {
   return (
-    <header className="flex justify-end items-center py-2 px-3 bg-white">
-      <div className="flex items-center gap-3">
-        <span>{username}</span>
-        <button className="bg-red-500 px-2 py-1 border border-gray-400 text-white rounded-xl" onClick={onLogout}>Logout</button>
-        <img src="https://i.pravatar.cc/40" alt="User avatar" className="rounded-full" />
-      </div>
+    <header className="flex justify-start items-center py-2 bg-gray-100 border-b">
+      <SidebarTrigger />
     </header>
   );
 }
