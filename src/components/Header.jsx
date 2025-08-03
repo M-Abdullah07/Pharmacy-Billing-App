f983a2d842bec8c360bbfe6c12403d7a23001496
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-08-03 23:30:50 +0500

    refactor: Header search bar removed also converted to using tailwindcss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
-// src/components/Header.jsx
 import React from "react";
-import "../styles/Header.css";
 
 export default function Header({ username, onLogout }) {
   return (
-    <header className="header">
-      <input className="search" type="text" placeholder="Search anything here..." />
-      <div className="user-info">
+    <header className="flex justify-end items-center py-2 px-3 bg-white">
+      <div className="flex items-center gap-3">
         <span>{username}</span>
-        <button className="logout-btn" onClick={onLogout}>Logout</button>
-        <img src="https://i.pravatar.cc/40" alt="User avatar" />
+        <button className="bg-red-500 px-2 py-1 border border-gray-400 text-white rounded-xl" onClick={onLogout}>Logout</button>
+        <img src="https://i.pravatar.cc/40" alt="User avatar" className="rounded-full" />
       </div>
     </header>
   );
