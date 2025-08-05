22de856872c8cd4315203a7ea569ef19c433e9cb
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-08-05 13:53:15 +0500

    feat: new sidebar pages and layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,63 +1,99 @@
 import React from "react";
-import { MdOutlineBackup } from "react-icons/md";
-import { RxDashboard } from "react-icons/rx"
-import { TfiPackage } from "react-icons/tfi";
-import { HiOutlineBuildingOffice, HiOutlineCreditCard, HiOutlineUser } from "react-icons/hi2"
-import { HiOutlineDocumentReport } from "react-icons/hi"
-import { IoLayersOutline, IoLocationOutline, IoLogOutOutline, IoPricetagOutline, IoSettingsOutline } from "react-icons/io5"
-import {
-  Sidebar,
-  SidebarHeader,
-  SidebarGroup,
-  SidebarContent,
-  SidebarFooter,
-  SidebarMenu,
-  SidebarGroupLabel,
-  SidebarGroupContent,
-  SidebarMenuItem,
-  SidebarMenuButton,
-  SidebarSeparator,
-} from "./ui/sidebar";
-import { Avatar, AvatarImage } from "./ui/avatar";
+import { HiOutlineClock } from "react-icons/hi2"
+import { IoSettingsOutline } from "react-icons/io5"
+import { BsArrowReturnLeft } from "react-icons/bs";
+import { MdOutlinePayments } from "react-icons/md";
+import { PiBuildingOffice, PiUsersDuotone } from "react-icons/pi";
+import { CiLocationOn } from "react-icons/ci";
+import { GoPackage } from "react-icons/go";
+import { LuFileCog } from "react-icons/lu";
+import { LiaUsersCogSolid } from "react-icons/lia";
+import { Sidebar, SidebarHeader, SidebarGroup, SidebarContent, SidebarFooter, SidebarMenu, SidebarGroupLabel, SidebarGroupContent, SidebarMenuItem, SidebarMenuButton, SidebarSeparator, } from "./ui/sidebar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { ChevronUp, CreditCard, Layers, LayoutDashboard, LogOut, NotebookText, SlidersIcon, Tag, UploadCloud, User2, Wallet } from "lucide-react";
+import "../index.css"
+import { Separator } from "./ui/separator";
 
-const menuItems = [
+const core = [
   {
     title: "Dashboard",
-    icon: RxDashboard,
+    icon: LayoutDashboard,
   },
   {
     title: "Add Sale",
-    icon: IoPricetagOutline,
-  },
-  {
-    title: "Companies",
-    icon: HiOutlineBuildingOffice,
-  },
-  {
-    title: "Products",
-    icon: TfiPackage,
-  },
-  {
-    title: "Areas",
-    icon: IoLocationOutline,
+    icon: Tag,
   },
   {
     title: "Add Batch",
-    icon: IoLayersOutline,
+    icon: Layers,
   },
   {
-    title: "Credit Dues",
-    icon: HiOutlineCreditCard,
+    title: "Return Handling",
+    icon: BsArrowReturnLeft,
+  },
+]
+
+const sidebar = [
+  {
+    title: "Inventory Items",
+    items: [
+      {
+        title: "Products",
+        icon: GoPackage,
+      },
+      {
+        title: "Stock Adjustment",
+        icon: LuFileCog,
+      },
+      {
+        title: "Expiry Management",
+        icon: HiOutlineClock,
+      },
+    ]
   },
   {
-    title: "Sales Reports",
-    icon: HiOutlineDocumentReport,
+    title: "Finance",
+    items: [
+      {
+        title: "Credit Dues",
+        icon: CreditCard,
+      },
+      {
+        title: "Customer Ledger",
+        icon: NotebookText,
+      },
+      {
+        title: "Supplier Payments",
+        icon: MdOutlinePayments,
+      },
+      {
+        title: "Expense Tracker",
+        icon: Wallet,
+      },
+    ]
   },
   {
-    title: "Customers",
-    icon: HiOutlineUser,
-  },
-];
+    title: "Stakeholders",
+    items: [
+      {
+        title: "Companies",
+        icon: PiBuildingOffice,
+      },
+      {
+        title: "Customers",
+        icon: PiUsersDuotone,
+      },
+      {
+        title: "Areas",
+        icon: CiLocationOn
+      },
+      {
+        title: "Salesmen",
+        icon: LiaUsersCogSolid,
+      },
+    ]
+  }
+]
 
 export default function AppSidebar({ onNavigate, onLogout, username }) {
   return (
@@ -69,60 +105,75 @@ export default function AppSidebar({ onNavigate, onLogout, username }) {
       </SidebarHeader>
 
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel> Vendor </SidebarGroupLabel>
+        <SidebarGroup className="-mt-2">
           <SidebarGroupContent>
             <SidebarMenu>
-              {menuItems.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild className="cursor-pointer">
-                    <a onClick={() => onNavigate(item.title)}>
-                      {item.icon && <item.icon />}
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+              {core.map((item) => (
+                <SidebarMenuButton asChild className="cursor-pointer">
+                  <a onClick={() => onNavigate(item.title)}>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+        {sidebar.map((group) => (
+          <SidebarGroup key={group.group}>
+            <SidebarGroupLabel>{group.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {group.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild className="cursor-pointer">
+                      <a onClick={() => onNavigate(item.title)}>
+                        {item.icon && <item.icon />}
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
       </SidebarContent>
 
       <SidebarFooter>
+        <SidebarSeparator className="mb-2" />
         <SidebarMenu>
-
           <SidebarMenuItem>
-            <SidebarMenuButton className="cursor-pointer" onClick={() => onNavigate("Backup & Export")}>
-              <MdOutlineBackup />
-              <span>Backup & Export</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-
-          <SidebarMenuItem>
-            <SidebarMenuButton className="cursor-pointer" onClick={() => onNavigate("Settings")}>
-              <IoSettingsOutline />
-              <span>Settings</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-
-          <SidebarMenuItem>
-            <SidebarMenuButton className="cursor-pointer" onClick={onLogout}>
-              <IoLogOutOutline />
-              <span>SignOut</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-
-          <SidebarMenuItem>
-            <SidebarSeparator className="mb-2 mt-2" />
-            <SidebarMenuButton>
-              <Avatar>
-                <AvatarImage src="https://i.pravatar.cc/20" />
-              </Avatar>
-              <span>{username}</span>
-            </SidebarMenuButton>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> {username}
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="top" className="anchor-width" >
+                <DropdownMenuItem onClick={() => onNavigate("Settings")}>
+                  <IoSettingsOutline />
+                  <span>Settings</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => onNavigate("Preferences")}>
+                  <SlidersIcon />
+                  <span>Prefrences</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => onNavigate("Backup")}>
+                  <UploadCloud />
+                  <span>Backup & Export</span>
+                </DropdownMenuItem>
+                <Separator className="mt-1 mb-1" />
+                <DropdownMenuItem onClick={onLogout}>
+                  <LogOut />
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
-    </Sidebar>
+    </Sidebar >
   );
 }
