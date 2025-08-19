db0f1486c512c091cb98893ea98ef77f297c009f
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-08-19 22:20:18 +0500

    feat: created new page for expiry management

--- src/pages/inventory/ExpiryManagement.jsx
+++ src/pages/inventory/ExpiryManagement.jsx
@@ -1,9 +1,58 @@
-export default function ExpiryManagement () {
+import { useState } from "react"
+import { Table, TableRow, TableHead, TableBody, TableCell, TableHeader } from "@/components/ui/table"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Search } from "lucide-react"
+
+export default function ExpiryManagement() {
+  const [loading, setLoading] = useState(false)
+
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <h1 >Expiry Management Page</h1>
-      <small className="text-sm leading-none font-medium text-muted-foreground"> Page Underconstruction </small>
-    </div>
+    <>
+      <div className="flex flex-col space-y-5">
+        <div className="flex gap-2 items-center">
+          <Search size={20} />
+          <Input
+            type="text"
+            placeholder="Search"
+            className="w-1/2"
+          >
+          </Input>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4">
+          {loading ? (
+            <div>Loading products...</div>
+          ) : (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Product Name</TableHead>
+                  <TableHead>Form</TableHead>
+                  <TableHead>Quantity</TableHead>
+                  <TableHead>Retail Price</TableHead>
+                  <TableHead>Shelf Location</TableHead>
+                  <TableHead>Expiry Date</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>xx</TableCell>
+                  <TableCell>xx</TableCell>
+                  <TableCell>xx</TableCell>
+                  <TableCell>xx</TableCell>
+                  <TableCell>xx</TableCell>
+                  <TableCell>xx</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan="11">
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          )}
+        </div>
+      </div>
+    </>
   )
 }
 
